344740798a2e5e4b6783919b605d3b4798fe0228
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-19 18:51:24 +0000

    added html and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yoochan Han
+// Created on:Mar 2024
 // This file contains the JS functions for index.html
+function enterClicked() {
+
+  // input
+
+  const firstName = document.getElementById("first-name").value
+
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+
+
+  // output
+
+  document.getElementById("user-info").innerHTML =
+
+    "Your info is: " + firstName + ", age " + userAge + "."
+
+}
