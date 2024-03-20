bf00d4f924bc88dce625d1426dc66d4dd77a6b13
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-20 18:23:12 +0000

    changed html, css, JS

--- js/script.js
+++ js/script.js
@@ -2,19 +2,10 @@
 // Created on:Mar 2024
 // This file contains the JS functions for index.html
 function enterClicked() {
-
   // input
-
   const firstName = document.getElementById("first-name").value
-
   const userAge = parseInt(document.getElementById("age-entered").value)
-
-
-
   // output
-
   document.getElementById("user-info").innerHTML =
-
     "Your info is: " + firstName + ", age " + userAge + "."
-
 }
